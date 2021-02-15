efef6a6d637b7dec4d4a4a103c1b53709826d242
lapedro.veloso <pedrofveloso@gmail.com>
2021-02-15 13:16:01 -0300

    feat: add fructose app project; add gitignore to ignore ds_store and xcuserstate files;

---
+++ Fructose/Fructose/FructoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructoseApp.swift
+//  Fructose
+//
+//  Created by lapedro.veloso on 15/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FructoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
