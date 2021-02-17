05f98307939c36287877684545c08ac12112c3b0
lapedro.veloso <pedrofveloso@gmail.com>
2021-02-17 13:02:41 -0300

    feat: create africa project; add resources and assets; add launch screen; add dark mode appearance;

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by lapedro.veloso on 17/02/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
