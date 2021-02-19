0502c78fa576c5218cde0c5e7efd868f2bb24c5f
lapedro.veloso <pedrofveloso@gmail.com>
2021-02-18 22:10:04 -0300

    feat: add video list and item views;

---
+++ Africa/Africa/Source/Video/VideoListItemView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListItemView.swift
+//  Africa
+//
+//  Created by lapedro.veloso on 18/02/21.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListItemView()
+    }
+}
---
+++ Africa/Africa/Source/Video/VideoModel.swift
@@ -0,0 +1,20 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by lapedro.veloso on 18/02/21.
+//
+
+import SwiftUI
+
+struct VideoModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoModel_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoModel()
+    }
+}
