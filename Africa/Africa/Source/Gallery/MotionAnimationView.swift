b1eb36e3a1744b88803c5a2eefa4e13a891699ea
lapedro.veloso <pedrofveloso@gmail.com>
2021-02-20 18:03:06 -0300

    feat: add motion animation view as the gallery background;

---
+++ Africa/Africa/Source/Gallery/MotionAnimationView.swift
@@ -0,0 +1,20 @@
+//
+//  MotionAnimationView.swift
+//  Africa
+//
+//  Created by lapedro.veloso on 20/02/21.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MotionAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MotionAnimationView()
+    }
+}
