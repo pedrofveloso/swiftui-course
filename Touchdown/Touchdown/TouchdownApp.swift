9163471920a49363af7ed94ac1f241dfa57a79bf
lapedro.veloso <pedrofveloso@gmail.com>
2021-02-22 11:51:30 -0300

    feat: create the touchdown project;

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by lapedro.veloso on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
