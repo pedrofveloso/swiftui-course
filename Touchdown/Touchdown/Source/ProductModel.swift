6c7a226fc6840259efe73db63b5c4e39bf91d281
lapedro.veloso <pedrofveloso@gmail.com>
2021-02-26 12:03:14 -0300

    feat: add product model based on the elements from product.list

--- Touchdown/Touchdown/Source/ProductModel.swift
+++ Touchdown/Touchdown/Source/ProductModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Product: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+    let price: Int
+    let description: String
+    let color: [Double]
+    
+    var redColorValue: Double {
+        color[0]
+    }
+    var greenColorValue: Double {
+        color[1]
+    }
+    var blueColorValue: Double {
+        color[2]
+    }
+    
+    var formattedPrice: String {
+        "$\(price)"
+    }
+    
+}
