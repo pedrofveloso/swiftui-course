b90573ad1a5978897f9260b405e37661b906de10
lapedro.veloso <pedrofveloso@gmail.com>
2021-02-28 18:19:53 -0300

    feat: create learn by doing project

---
+++ Learn by Doing/Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by lapedro.veloso on 28/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
