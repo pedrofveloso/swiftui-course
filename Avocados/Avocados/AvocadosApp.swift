806f8d0a21f68f0f7776c342f9ac22a375837774
Pedro Veloso <pedrofveloso@gmail.com>
2021-03-06 19:11:29 -0300

    feat: create avocados project and add resources

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by lapedro.veloso on 06/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
